32c3c0ca4e841b11c354174a1a5698749b9563a7
yilma <yilma@DESKTOP-U1LH654>
2023-04-25 17:37:42 +0300

    Add project files.

---
+++ 250423_NBUY2/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace _250423_NBUY2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ColumnHeader UrunAdi;
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.BirimFiyati = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            UrunAdi = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(84, 294);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(240, 294);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 2;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.BirimFiyati,
+            UrunAdi});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(84, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(277, 265);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // UrunAdi
+            // 
+            UrunAdi.Tag = "UrunAdi";
+            UrunAdi.Text = "BirimFiyati";
+            UrunAdi.Width = 131;
+            // 
+            // BirimFiyati
+            // 
+            this.BirimFiyati.Text = "UrunAdi";
+            this.BirimFiyati.Width = 117;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader BirimFiyati;
+    }
+}
+
---
+++ 250423_NBUY2/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _250423_NBUY2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int flag = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection("Server=localhost;Database=Northwind;User=sa;Pwd=123");
+            SqlCommand cmd = new SqlCommand("select * from Urunler");
+            cmd.Connection = conn;
+            try
+            {
+                conn.Open();
+                SqlDataReader reader= cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    listView1.Items.Add(reader["UrunAdi"].ToString());
+                    listView1.Items[flag].SubItems.Add(reader["BirimFiyati"].ToString());
+                    comboBox1.Items.Add(reader["UrunAdi"].ToString());
+                    comboBox2.Items.Add(reader["BirimFiyati"].ToString());
+                    flag++;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
